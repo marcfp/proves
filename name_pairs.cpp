0a90c2fe2e1b04723ca0d5b66a625b7e9af2efd8
Marcfp <marcfurtia@riseup.net>
2018-01-27 12:57:08 +0100

    name pairs, ex3, theme 9
    understand wrong sort function, i must implefilmnent it from source, not using c++ function
    Updated name_pairs.h file too.

--- name_pairs.cpp
+++ name_pairs.cpp
@@ -1,29 +1,104 @@
-#include<iostream>
-#include<stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <stdexcept>
-#include <cmath>
-#include <string>
-#include <sstream>
-#include<std_lib_facilities.h>
+#include <string.h>
+//#include <functional>
+
+
+#include "name_pairs.h"
 
 using namespace std;
-#define debug 1
 
 
-class Name_pairs{
-		vector<string> name;
-		vector<double> age;
-	public:
-		Name_pairs(string name, double age);
-		void read_names(string name);
-		void read_ages(double age);
-}np;
+void Name_Pairs::read_names(){// return int per printar ?
+	string nome;
+	string zero="0";
+	while(nome.compare(zero)!=0) {
+		cout << endl << "Llegeix noms" << endl;
+		cout << endl << "Introdueix el nom : " ;
+		cin >> nome;
+		set_nom(nome);
+	}
+};
+void Name_Pairs::set_nom(string nome){
+	name.push_back(nome);
+}
+
+void Name_Pairs::set_age(double edat){
+	age.push_back(edat);
+}
+
+void Name_Pairs::read_ages(){ //return int per printar ?
+	double edat;
+
+	cout << endl << endl << endl << endl  << "Llegeix edats" << endl << endl << endl << endl;
+	double val = name.size();
+	vector<string>::iterator it_nom = name.begin();
+	while(val >1){ 
+		cout << endl << "Introdueix l'edat  de "<< *it_nom << " : " ;
+		it_nom++;
+		cin >> edat;
+		set_age(edat);
+		val--;
+	}
+	
+};	
+
+void Name_Pairs::print(){
+	vector<string>::iterator it_nom = name.begin();
+	cout << endl << "Escriure dades(ordenadas alfabéticament A .. Z)" << endl;
+	vector<double>::iterator it_age = age.begin();
+
+	for(; it_nom != name.end()-1; it_nom++){
+		cout << "Nom = " << *it_nom << endl;
+		cout << "edat = " << *it_age << endl;
+		it_age++;
+	}
+}
 
 
+void Name_Pairs::sort()
+{
+	for (int i = 0; i < name.size()-1; i++)
+	{
+		for (int j = 0; j < name.size()-1; j++)
+		{
+			if (i != j)
+			{
+				string t0 = name[i];
+				string t1 = name[j];
+				if (t0 < t1)
+				{
+					string t = name[i];
+					name[i] = name[j];
+					name[j] = t;
+
+					double a = age[i];
+					age[i] = age[j];
+					age[j] = a;
+				}
+			}
+		}
+	}
+}
 
 
+int main () {
+	Name_Pairs np;
+
+	cout << endl << endl << "NAME-PAIRS_NOU" << endl << endl;
+
+	cout << "Llegeix dades, primer es llegeixen els noms, després es llegeixen les edats, per finalitzar els nom, escriu 0, es llegiran el mateix nombre d'edats" << endl;	
+	np.read_names();
+	np.read_ages();
+
+	np.print();
+//	cout << endl << "Ordena" << endl;
+	np.sort();
+//	cout << endl << "Ordenació feta" << endl;
+	np.print();
+
+  return (0);
+
+}
+ 
--- name_pairs.h
+++ name_pairs.h
@@ -3,16 +3,25 @@
 using namespace std;
 class Name_Pairs {
   public:
-      string name;
-      double age;
     //setters
     void set_nom(string);
     void set_age(double);
-    void print();
     
+   void print();
+    
+    
+   void read_names();
+   void read_ages();
+   
+
+   void sort();    
     //getter
 //    void get_values(vector<Name_Pairs>);
-    
+//   private:
+//	vector<double>::iterator  it_age = age.begin();
+//	vector<double>::iterator it_age_fi = age.end();
+	vector<string> name;
+	vector<double> age; 
 };
 
 
