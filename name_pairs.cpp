6de8f1eeb2d471832ff182090129ff8a6e7ed517
Marcfp <marcfurtia@riseup.net>
2018-02-05 19:34:08 +0100

    ex3 theme 9 maked
    exercice 3 "Replace Name_pair::print() with a (global) operator << and define == and != for
    Name_pairs." Maked

--- name_pairs.cpp
+++ name_pairs.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -61,7 +62,58 @@ void Name_Pairs::print(){
 	}
 }
 
+ostream & operator << (ostream & os,  Name_Pairs& np) //print
+{
 
+	if(np.name.size()==0) {
+		return os <<  "no hi ha res a mostrar" ;
+	}
+	else{
+		for(int i=0; i <= np.name.size()-2;i++){
+			os << "nom = " << np.name[i] << " , edat = " << np.age[i] << endl; 
+		}
+	}
+}
+
+ostream & operator == (Name_Pairs np1, Name_Pairs & np2) // iguals?
+{
+	if(np1.name.size() == np2.name.size()) {
+		cout << endl << "IGUALS mides ;) anem bé!" << endl;
+		for(int i=0; i<np1.name.size()-1;++i){
+			if(np1.name[i]!= np2.name[i]){ 
+						cout << endl << "NOMS Diferents! fora!!!" << endl;
+						 break;
+			}
+			else cout << endl << np1.name[i] << " = " << np2.name[i] ;
+		 	if(np1.age[i] != np2.age[i]) {
+						cout << endl << "EDATS Diferents! Fora!!!!" << np2.age[i] << "!=" << np1.age[i] << "?" << endl;
+						break;
+			}else cout << endl << ", " << np1.age[i] << " = " << np2.age[i] << " igual!! " << endl;
+		}
+	}
+	else{
+		cout << endl <<"MIDES DIFERENTS" << endl;
+	}
+}
+ostream & operator != (Name_Pairs np1, Name_Pairs & np2) // diferents?
+{
+	if(np1.name.size() != np2.name.size()) {
+		cout << endl << "Mides Diferents" << endl;
+	}
+	else {
+		for(int i=0; i<np1.name.size()-1; ++i){
+			if(np1.name[i]== np2.name[i]){
+				cout << endl << "noms iguals np1.name[i]= "  <<  np1.name[i] << "np2.name[i] = " << np2.name[i] << endl;
+			}
+			else cout << endl << np1.name[i] << " == " << np2.name[i] << endl;
+			if(np1.age[i]== np2.age[i]){
+                                 cout << endl << "ages iguals np1.age[i]= "  <<  np1.age[i] << "np2.age[i] = " << np2.age[i] << endl;
+                        }
+                        else cout << endl << np1.age[i] << " == " << np2.age[i] << endl;
+		}
+		
+	}
+}
 void Name_Pairs::sort()
 {
 	for (int i = 0; i < name.size()-1; i++)
@@ -98,14 +150,15 @@ int main () {
 	np.read_ages();
 	
 	cout << endl << "Dades introduides sense ordenar " << endl;
-	np.print();
+	cout << np;
+//	np.print();
 //	cout << endl << "Ordena" << endl;
 	np.sort();
 
 	cout << endl << "Dades introuides irdenades alfabéticament" << endl;
-	np.print();
-
+//	np.print();
+	cout << np;
+	np == np;
   return (0);
 
 }
- 
