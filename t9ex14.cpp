3c46c5eea7623c1344ecaa5d08b1762a68ac8642
Marcfp <marcfurtia@riseup.net>
2018-03-09 16:02:22 +0100

    t9ex14 maked
    t9ex14 maked

--- t9ex14.cpp
+++ t9ex14.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 #include "t9ex14.h"
 
-Money Money::operator + (const Money& m2) const{
-Money m;
-m.valor_total=this->valor_total+m2.valor_total;
-return (m);
+Money Money::operator/ (const Money& m2) const{
+	Money m;
+	m.valor_total = this->valor_total / m2.valor_total;
+	return(m);
+}
+
+Money Money::operator* (const Money& m2) const{
+	Money m;
+	m.valor_total = this->valor_total * m2.valor_total;
+	return(m);
+}
+
+Money Money::operator- (const Money& m2) const{
+	Money m;
+	m.valor_total=this->valor_total - m2.valor_total;
+	return(m);
+}
+
+Money Money::operator+ (const Money& m2) const{
+	Money m;
+	m.valor_total=this->valor_total + m2.valor_total;
+
+	return (m);
 }
 
 int main(){
@@ -18,26 +38,26 @@ cout << "14. Design and implement a Money class for calculations involving dolla
 	<< " cents in a long int, but input and output as dollars and cents, e.g., $123.45. Do not "
 	<< " worry about amounts thatdon’t fit into a long int" << endl;
 	cout << endl << "###################################################" << endl;
-	cout << endl << "valor_total = " << m1.getValor_total() << endl ; //", part entera = " << m1.part_entera << ", part decimal = " << m1.part_decimal << "." << endl;
+	cout << endl << "valor_total = " << setprecision(2) << fixed << m1.getValor_total() << endl ; //", part entera = " << m1.part_entera << ", part decimal = " << m1.part_decimal << "." << endl;
 	cout << endl << "###################################################" << endl;
 	cout << endl << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
 	m1.setValor();
 	cout << endl << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << endl;
 	cout << endl << "###################################################" << endl;
-	cout << m1.getValor_total() << endl;
+	cout << setprecision(2) << fixed << m1.getValor_total() << endl;
 	cout << endl << "###################################################" << endl;
 	m2.setValor();
 	cout << m2.getValor_total();
  
-	
-	cout << endl << "Ha entrat algu a m3?" << endl;
+	cout << endl << "operacions" << endl;	
 	m3=m1+m2;
-	cout << m3.getValor_total();
-/*	cout << endl << "entra i " << endl;
-	cin >> i;
-	m1.getValor_total()*i;
-	cout << "despres de multipliciar " << endl;
-	cout << m1.getValor_total() << endl;*/
+	cout << " suma de : " << setprecision(2) << fixed << m1.getValor_total() << " i " << m2.getValor_total() << " = "<< m3.getValor_total() << endl;
+	m3=m1-m2;
+	cout << " resta de : "<< setprecision(2) << fixed << m1.getValor_total() << " i " << m2.getValor_total() << " = " << m3.getValor_total() << endl;
+	m3=m1*m2;
+	cout << " Multiplicació de : "<< setprecision(2) << fixed << m1.getValor_total() << " i "<< m2.getValor_total() << " = " << m3.getValor_total() << endl;
+	m3=m1/m2; 
+	cout << " Divisió de : " << setprecision(2) << fixed << m1.getValor_total() << " i " << m2.getValor_total() << " = " << m3.getValor_total() << endl;
 	cout << endl;
 return(0);
 }
--- t9ex14.h
+++ t9ex14.h
@@ -1,42 +1,23 @@
 class Money{
-	float valor_total;
-	int part_entera;
-	int part_decimal;
+	long double valor_total;
 public:
 	Money(){
 		valor_total=0;
-		part_entera=0;
-		part_decimal=0;
-		//this->valor_total=0;
 	}
 	void setValor(){
 		float valor_total1;
 		cout << endl << "Entra un valor " << endl;
 		cin >> valor_total1;
-		valor_total=valor_total1;
+		valor_total= valor_total1;
 	}
 	float getValor_total(){
-//		cout << endl << "El valor entrat és " << valor_total << endl;
 		return(valor_total);
 	}
 	 
 	~Money(){}
 
-	Money operator + (const Money& m2) const;
-/*	{
-		Money m;
-		m.valor_total=this->valor_total+m2.valor_total;
-		return (m);
-		Money resultat;
-		resultat.valor_total=m1.valor_total+m2.valor_total;
-		return resultat;
-
-		return(m1.valor_total+m2.valor_total);
-	}
-	Money& operator * (const float& i){
-		Money m;
-		m.valor_total=i*(this->valor_total);
-		return(m);
-	}*/
-};
- 
+	Money operator+ (const Money& m2) const;
+	Money operator- (const Money& m2) const;
+	Money operator* (const Money& m2) const;
+	Money operator/ (const Money& m2) const;
+}; 
