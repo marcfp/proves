51e62cd9edae820daa431a2c1f0a4fb9a6b58c78
Marcfp <marcfurtia@riseup.net>
2018-03-18 20:09:30 +0100

    updated sources of theme 9 ex 15, but not finished
    updated sources of theme 9 ex 15, but not finished

--- t9ex15.h
+++ t9ex15.h
@@ -48,17 +48,56 @@ http://en.cppreference.com/w/cpp/string/basic_string/erase
 */				
 				float resultat_suma;
 				if(pos_suma < parametres.size() ) {
-if(debug==0)	cout << "parametres = "<< parametres << " parametres.length() = " << parametres.length() << "pos_suma = " << pos_suma << " pos_resta = " << pos_resta << " parametres.size() = " << parametres.size() << " parametres[7]= " << parametres[7]<< " parametres.compare1(\"USD\")=" <<  parametres.compare("USD")<< " parametres.compare2(\"USD\")=" <<  parametres.compare("USD")<< endl ; 
+if(debug==0)	cout << "parametres = "<< parametres << " parametres.length() = " << parametres.length() << "pos_suma = " << pos_suma << " pos_resta = " << pos_resta << " parametres.size() = " << parametres.size() << " parametres[7]= " << parametres[7]<< " parametres.compare1(\"USD\")=" <<  parametres.compare("USD")<< " parametres.compare2(\"USD\")=" <<  parametres.compare("USD")<< "pos_inici = " << parametres.substr(0,pos_suma) << "pos_fi =" << parametres.substr(pos_suma+1, parametres.length()) <<  endl;
 
 /*crear 2 substring a partir del +*/
-                        string str_sumafi = parametres.substr(pos_suma+1);
-                        cout << endl << "str_sumafi = " << str_sumafi << endl;//segon parametre
-                        string str_sumainici = parametres.substr(0 , pos_suma);//primer parametre
-                        string troba = parametres.substr(parametres.length());
-                        cout << endl << "str_sumainici = " << str_sumainici << "\n troba = " << troba << endl;
+                    string cad_inici ="";
+                    
+                    string cad_fi = ""; 
+                    int pos_USD1, pos_DKK1, pos_USDfi, pos_DKKfi;
+                    cad_inici=parametres.substr(0,pos_suma);
+                    cad_fi=parametres.substr(pos_suma+1, parametres.length());
+                    cout << endl << endl << endl << "cad_inici = " << cad_inici << "\ncad_fi =" << cad_fi <<  endl << " cad_inici.find(\"USD\")="<< cad_inici.find("USD") << endl << "cad_fi.find(\"USD\")="<< cad_fi.find("USD") << endl;
+                    
+                    /* buscar la moneda de cada substring*/
+                           /*aquí*/
+                    if(cad_inici.find("USD")>parametres.length()){ 
+                        pos_DKK1=cad_inici.find("DKK");
+                        if(pos_DKK1==-1) cout << endl << "ERROR!! ABORT!!! DKK1 FAILS"<< endl;
+                        if(pos_DKK1<parametres.length()){
+                                cout << "calculo valor dkk" << endl;
+                                
+                        }
+                    }
+                        else {
+                            pos_USD1=cad_inici.find("USD");
+                            if(pos_USD1==-1) cout << endl << "ERROR!! ABORT!!! USD1 FAILS"<< endl;
+                            if(pos_USD1<parametres.length()){
+                                cout << "calculo valor usd" << endl;
+                            }
+                        }
+                    
+                    if(cad_fi.find("USD")>parametres.length()){ 
+                        pos_DKKfi=cad_fi.find("DKK");
+                        if(pos_DKKfi==-1) cout << endl << "ERROR!! ABORT!!! DKKfi FAILS"<< endl;
+                        if(pos_DKKfi<parametres.length()){
+                                cout << "calculo valor dkk fi" << endl;
+                        }
+                    }
+                        else {
+                            pos_USDfi=cad_fi.find("USD");
+                            if(pos_USD1==-1) cout << endl << "ERROR!! ABORT!!! USD1 FAILS"<< endl;
+                            if(pos_USDfi<parametres.length()){
+                                cout << "calculo valor usd fi" << endl;
+                            }
+                        }
+                    cout << endl << "pos_DKK1 = " << pos_DKK1 << " pos_USD1 = " << pos_USD1 << endl;
+                    cout << endl << "pos_DKKfi = " << pos_DKKfi << " pos_USDfi = " << pos_USDfi << endl;
+                    
+                    
+                                               
                         
-                        /* buscar la moneda de cada substring*/
-//                         /*aquí*/
+//                  
                         /*si tots 2 tenene moneda correcta, fer càcul, sino llençar error*/
                         
 						/*string::size_type indexUSD1fi;
@@ -167,7 +206,8 @@ if(debug==0)                                            cout << endl << "+ calcu
 				
 				
 				if(parametres.find("-") < parametres.size() ) {
-if(debug==0)	cout << "parametres = "<< parametres ; 
+                }
+/*if(debug==0)	cout << "parametres = "<< parametres ; 
 						string::size_type indexUSD1fi;
 						string sub1ll;
 						string::size_type indexUSD1 = parametres.find("USD");
