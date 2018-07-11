3bbc3dc53cf6e8e48fb56f4411b94b6b8ff95930
Marcfp <marcfurtia@riseup.net>
2018-07-11 22:56:46 +0200

    updated somebugs
    bug solucionated, now it doesn't accept ixi number, it throws en error of mal formed number

---
+++ calcul_numeros_romans.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+//Falta revisar i acabar de comprobar
+// constexpr function for product of two numbers. 
+// By specifying constexpr, we suggest compiler to  
+// to evaluate value at compiler time 
+constexpr int to_int(const char lletra)
+    {
+        return (lletra == 'M') ? 1000
+        : (lletra == 'D') ? 500
+        : (lletra == 'C') ? 100
+        : (lletra == 'L') ? 50
+        : (lletra == 'X') ? 10
+        : (lletra == 'V') ? 5
+        : (lletra == 'I') ? 1
+        : 0;
+    }
+    
+int debug=0;
+class Roma{
+
+  int valor;
+  char lletra;
+  string num;
+  public:
+    int canvi_lletra_a_valor(){
+      return(to_int(lletra));
+    }
+    
+    void set_num(){
+      cout << "Entra el numero en xifres romanes i et diré el seu valor en números arábics º1" << endl;
+      cin >> num;
+      for(int i=0; i<num.size();++i) num[i]=toupper(num[i]);
+    }
+    
+    string get_num(){
+      return num;
+    }
+    
+    
+    void set_lletra(){
+      cout << "Entra la lletra " << endl;
+      cin >> lletra;
+      if(islower(lletra)) lletra=toupper(lletra);
+    }
+    
+    char get_lletra(){
+      return lletra;
+    }
+    
+    int as_int(){ 
+      int val=0;
+      int anterior=0;
+      int conta=0;
+      int suma;
+      if(debug==1)cout << "valor abans for val  = " << val << endl;
+      val=to_int(num[0]);
+      if(debug==1)cout << "valor val  = " << val << endl;
+      conta++;
+      for(int i=1;i<num.size();++i){
+        if(conta<=3 && conta<=num.size()){ 
+	  if(debug==1)cout  << " val = " << val << " to_int(num[i]) = " << to_int(num[i]) << "\n anterior = " << anterior << " i = " << i << " conta = " <<  conta <<  endl;
+	  if(conta==1){ 
+	      anterior=to_int(num[i]);
+	     // val=val+to_int( num[i]);
+	  }
+	  else anterior=to_int(num[i-1]);
+	  if(val<to_int(num[i]) && conta==1)  val=to_int(num[i])-val; 
+	  else if(conta<=3 || anterior==to_int(num[i] && to_int(num[0]) == to_int(num[2]) && to_int(num[1])==10)) {
+		    if(conta <=3 && val>=to_int(num[i]) && anterior<val) val=to_int(num[i])+val; //comproba que el número no estigui mal format (restant)
+		    else{
+		      if(debug==1)cout << "to_int(num[0]) = " << to_int(num[0])<< " to_int(num[1]) = " << to_int(num[1]) << " to_int(num[2]) = " << to_int(num[2]) << endl;
+		      if(to_int(num[0]) == to_int(num[2]) && to_int(num[1])==10) {
+			cout << "Número mal format" << endl;
+			return(0);
+		      }else{
+			cout << "Número mal format" << endl;
+			return(0);
+		      }
+		      
+		    }
+		    anterior=to_int(num[i]);
+		    if(conta==2 &&  ((to_int(num[i])==5 && to_int(num[i-1])==5) || (to_int(num[i])==50 && to_int(num[i-1])==50)|| (to_int(num[i])==500 && to_int(num[i-1])==500))){
+                      if(debug==1)cout << "Número erroni, no es pot repetir!!" << endl;
+                      return (-15);
+                      
+                    }
+
+		}
+		else {
+		  if(to_int(num[i])!=to_int(num[i-1])) conta=0;
+		  else{
+		   if(debug==1)cout << "error reiniciant conta ?" << endl;
+		   return (0);
+		  }
+		  
+		}
+		conta++;
+		if(conta>3 && to_int(num[i])==to_int(num[i-1]) && to_int(num[i])==to_int(num[i-2])&& to_int(num[i])==to_int(num[i-3])) {
+                    if(debug==1)cout << " conta final = " << conta << " números repetits" << endl;
+		    conta=0;
+                    return (0);
+		  
+                }
+	}
+	else {
+	  /*problema del 128 es aqui ?*/
+	  if(debug==1)cout << endl<< endl<< endl<< "passa aqui el 128 ?" << endl<< endl<< endl<< endl;
+	  if(to_int(num[i])==anterior && conta<=3){	    
+	    if(to_int(num[i-1])<to_int(num[i]) && conta==1 )  val=val-to_int(num[i]); 
+	    else if(conta<=3) val=val+to_int(num[i]);
+		  else {
+		    val=0;
+		      cout << "número mal format!!" << endl;
+		      return(0);
+		  }
+	    anterior=to_int(num[i]);
+	    //if(to_int(num[i])>val){     
+	    //  return (0);
+	    //}
+	  }
+	  else {
+	    if(debug==1)cout << " to_int(num[i]) = " << to_int(num[i]) << " to_int(num[i-1]) = " << to_int(num[i-1]) << " i = " <<  i << " num.size() = " << num.size() << " val = " << val << endl;
+	    if(to_int(num[i])<=to_int(num[i-1]) && i<num.size()){ // per provar ....
+	      if(conta <=3)val=val+to_int(num[i]);	       //per provar a veure si sumo ...
+	      else { 
+		    cout << "número mal format!! "<< endl;
+		    return (0);
+	      }
+	    }	      
+	    else {
+		if(debug==1)cout << endl << "número mal format dins else" << endl;
+		conta=0;
+		return (0);
+	    }
+	  }
+	
+	}
+        //mirar si s'ha de sumar o restar, a part d'això, també s'ha de mirar si el número entrat és correcte.
+        if(debug==1)cout << "i = " << i << "num[" << i <<"] = " << num[i] << " =" << to_int(num[i]) << "  val = " << val << " conta = " << conta << endl;
+	if(to_int(num[i-1])>val && to_int(num[0])==to_int(num[2])) {
+	  cout << "número mal format" << endl;
+	  return (0);
+	}
+      }
+      valor=val;
+      if(debug==1)cout << " get_valor -> valor  = " << valor << endl;
+      
+      return (valor);
+    }	
+};
+    
+  
+int main() 
+{ 
+   Roma roma;
+    
+    roma.set_num();
+    if(roma.as_int()!=-15) cout << " Roman " << roma.get_num() << " equals " << roma.as_int() << endl;
+    else cout << " número mal format!!!" << endl;
+   
+    return (0); 
+}
