c68ee3fafa98a4ba3393b4761dbcd00f8a2be56e
Marcfp <marcfurtia@riseup.net>
2018-07-20 16:10:28 +0200

    updated roman number calculator but not finished
    updated but not finished roman number calculator

--- calcul_numeros_romans.cpp
+++ calcul_numeros_romans.cpp
@@ -1,154 +1,141 @@
 #include <iostream>
-#include <ctype.h>
-using namespace std;
-//Falta revisar i acabar de comprobar
-// constexpr function for product of two numbers. 
-// By specifying constexpr, we suggest compiler to  
-// to evaluate value at compiler time 
-constexpr int to_int(const char lletra)
-    {
-        return (lletra == 'M') ? 1000
-        : (lletra == 'D') ? 500
-        : (lletra == 'C') ? 100
-        : (lletra == 'L') ? 50
-        : (lletra == 'X') ? 10
-        : (lletra == 'V') ? 5
-        : (lletra == 'I') ? 1
-        : 0;
-    }
-    
-int debug=1;
-class Roma{
 
-  int valor;
-  char lletra;
-  string num;
-  public:
-    int canvi_lletra_a_valor(){
-      return(to_int(lletra));
-    }
-    
-    void set_num(){
-      cout << "Entra el numero en xifres romanes i et diré el seu valor en números arábics º1" << endl;
-      cin >> num;
-      for(int i=0; i<num.size();++i) num[i]=toupper(num[i]);
-    }
-    
-    string get_num(){
-      return num;
-    }
-    
-    
-    void set_lletra(){
-      cout << "Entra la lletra " << endl;
-      cin >> lletra;
-      if(islower(lletra)) lletra=toupper(lletra);
-    }
-    
-    char get_lletra(){
-      return lletra;
-    }
-    
-    int as_int(){ 
-      int val=0;
-      int anterior=0;
-      int conta=0;
-      int suma;
-      if(debug==1)cout << "valor abans for val  = " << val << endl;
-      val=to_int(num[0]);
-      if(debug==1)cout << "valor val  = " << val << endl;
-      conta++;
-      for(int i=1;i<num.size();++i){
-        if(conta<=num.size()){ 
-	  
-	  anterior=to_int(num[i-1]);
-	  
-	  if(debug==1)cout  << " val = " << val << " to_int(num[i]) = " << to_int(num[i]) << "\n anterior = " << anterior << " i = " << i << " conta = " <<  conta <<  endl;
-	  
-	  if(val<to_int(num[i]) && conta==1)  val=to_int(num[i])-val; 
-	  else if(conta<=num.size() || anterior==to_int(num[i] && to_int(num[0]) == to_int(num[2]) && to_int(num[1])==10)) {
-		    if(conta <=num.size() && val>=to_int(num[i]) && anterior<=val) val=to_int(num[i])+val; //comproba que el número no estigui mal format (restant)
-		    else{
-		      if(debug==1)cout << "to_int(num[0]) = " << to_int(num[0])<< " to_int(num[1]) = " << to_int(num[1]) << " to_int(num[2]) = " << to_int(num[2]) << endl;
-		      if(to_int(num[0]) == to_int(num[2]) && to_int(num[1])==10) {
-			cout << "Número mal format" << endl;
-			return(0);
-		      }	      
-		    }
-		    anterior=to_int(num[i]);
-		    if(conta==2 &&  ((to_int(num[i])==5 && to_int(num[i-1])==5) || (to_int(num[i])==50 && to_int(num[i-1])==50)|| (to_int(num[i])==500 && to_int(num[i-1])==500))){
-                      if(debug==1)cout << "Número erroni, no es pot repetir!!" << endl;
-                      return (-15);                      
-                    }
-		}
-		else {
-		  if(to_int(num[i])!=to_int(num[i-1])) conta=0;
-		  else{
-		   if(debug==1)cout << "error reiniciant conta ?" << endl;
-		   return (0);
-		  }
-		  
-		}
-		conta++;
-		if(conta>3 && to_int(num[i])==to_int(num[i-1]) && to_int(num[i])==to_int(num[i-2])&& to_int(num[i])==to_int(num[i-3])) {
-                    if(debug==1)cout << " conta final = " << conta << " números repetits" << endl;
-		    conta=0;
-                    return (0);
-		  
-                }
-	}
-	else {
-	  /*problema del 128 es aqui ?*/
-	  if(debug==1)cout << endl<< endl<< endl<< "passa aqui el 128 ?" << endl<< endl<< endl<< endl;
-	  if(to_int(num[i])==anterior && conta<=3){	    
-	    if(to_int(num[i-1])<to_int(num[i]) && conta==1 )  val=val-to_int(num[i]); 
-	    else if(conta<=3) val=val+to_int(num[i]);
-		  else {
-		    val=0;
-		      cout << "número mal format!!" << endl;
-		      return(0);
-		  }
-	    anterior=to_int(num[i]);
-	  }
-	  else {
-	    if(debug==1)cout << " conta = " << conta <<  " to_int(num[i]) = " << to_int(num[i]) << " to_int(num[i-1]) = " << to_int(num[i-1]) << " i = " <<  i << " num.size() = " << num.size() << " val = " << val << endl;
-	    if(to_int(num[i])<=to_int(num[i-1]) && i<num.size()){ // per provar ....
-	      if(conta <num.size())val=val+to_int(num[i]);	       //per provar a veure si sumo ...
-	      else { 
-		    cout << "número mal format!! "<< endl;
-		    return (0);
-	      }
-	    }	      
-	    else {
-		if(debug==1)cout << endl << "número mal format dins else" << endl;
-		conta=0;
-		return (0);
-	    }
-	  }
-	
-	}
-        //mirar si s'ha de sumar o restar, a part d'això, també s'ha de mirar si el número entrat és correcte.
-        if(debug==1)cout << "i = " << i << "num[" << i <<"] = " << num[i] << " =" << to_int(num[i]) << "  val = " << val << " conta = " << conta << endl;
-	if(to_int(num[i-1])>val && to_int(num[0])==to_int(num[2])) {
-	  cout << "número mal format" << endl;
-	  return (0);
+#include <string>
+#include <iconv.h>
+
+using namespace std;
+
+int debug=1; //1 debug 0 net
+
+int roman2decimal(const string& roman) //assume roman is uppercase
+{
+   int result = 0;
+   const int LEN = roman.length();
+   int conti=0;
+   int contx=0;
+   //falta contl;
+   int contc=0;
+   int contm=0;
+   int cont5m=0;
+   
+   string utfV="\u01B0"; //Ѵ <- control+shit+u+0474
+   string utfm="\u026F"; //ɯ <- control+shit+u+026f
+   string utfd="\u0256"; //ɖ <- control+shit+u+0256
+   setlocale(LC_ALL, "ca_ES.UTF-8");
+   cout << " roman = " << roman <<  " LEN = " << LEN << "utfV = "<< "\u01B0" << " utfm= " << utfm << " utfd= " << utfd  << endl;
+   //cout << "utf8 utfV.c_str()=" << utfV.c_str() << endl;
+   for (int i = 0; i < LEN; ++i)
+   {
+     if(roman.compare(utfV)==0) {cout << endl << endl << " lletra del 5000 trobat" << endl << endl; result+=5000; return result;}
+      if (roman[i] == 'I' && i != LEN-1)
+      {
+         if      (roman[i+1] == 'V' && roman[i-1]!='I') { result +=    4; cout << "I suma 4 result =" << result << endl; i++; } //IV leap V char
+         else if (roman[i+1] == 'X') { result +=    9; cout << "I suma 9 result = " << result << endl;i++; } //IX leap X char
+         else if (roman[i+1] == 'I' ) { 
+	   result +=    1; 
+	   conti++;
+	   if(conti>=3) { cout << "Masses lletres I seguides!!!" << endl; return 0; }
+	   if(debug==1)cout << "I suma 1 result =" << result << endl;	   
 	}
       }
-      valor=val;
-      if(debug==1)cout << " get_valor -> valor  = " << valor << endl;
+      else if (roman[i] == 'X' && i != LEN-1)
+      {
+	cout << " roman.find('X') = " << roman.find('X') << " \t roman.rfind('X') =" << roman.rfind('X') << endl;
+         //if      (roman[i-1] != 'L' &&roman[i+1] == 'L'  && i-1>=0) { result +=   40; i++; } //XL leap L char
+         if      (roman[i+1] == 'L'/* && roman.find('X')==roman.rfind('X')*/) { 
+	   result +=   40; 
+	   i++; 	   
+	} //XL leap L char
+         else if(roman[i+1] == 'L' && roman.find('X')!=roman.rfind('X')){
+	   cout << "mal format L" << endl; 
+	   return 0;	   
+	}
+	   else if (roman[i+1] == 'C') { result +=   90; i++; } //XC leap C char
+         else                        { 
+	   result +=   10;      
+	   contx++;
+	   if(contx>=4) { cout << "Masses lletres X seguides!!!" << endl; return 0; }
+	   if(debug==1)cout << "X suma 10 result =" << result << endl;
+	}
+      }
+      else if (roman[i] == 'C' && i != LEN-1)
+      {
+         if      (roman[i+1] == 'D') { result +=  400; i++; } //CD leap D char
+         else if (roman[i+1] == 'M') { result +=  900; i++; } //CM leap M char
+         else if (roman[i+1] == 'L') { result +=  150; if(roman[i+2]!='L') i++; }
+         else                        { 
+	   result +=  100;      
+	   contc++;
+	   if(contc>=4) { cout << "Masses lletres C seguides!!!" << endl; return 0; }
+	   if(debug==1)cout << "C suma 100 result =" << result << endl;	   
+	}
+      }
+      else if (roman[i] == 'M' && i != LEN-1)
+      {
+         if      (roman[i+1] == 'D') { result +=  1500; i++; } //CD leap D char
+         else if (roman[i+1] == 'L') { result +=  1050; if(roman[i+2]!='L') i++; }
+         else if (roman[i+1] == '\u01B0') { result +=  4000; if(roman[i+2]!='L') i++; }
+         else                        { 
+	   result +=  1000;      
+	   contm++;
+	   if(contm>=3) { cout << "Masses lletres M seguides!!!" << endl; return 0; }
+	   if(debug==1)cout << "M suma 1000 result =" << result << endl;	   
+	}
+      }else if (roman[i] == 
+	'\u01B0' && i != LEN-1)
+      {
+	cout << " Entro dins de comparació \"\u01B0\" " << endl;
+         if      (roman[i+1] == 'D') { result +=  5500; i++; } //CD leap D char
+         else if (roman[i+1] == 'L') { result +=  5050; if(roman[i+2]!='L') i++; }
+         else                        { 
+	   result +=  5000;      
+	   cont5m++;
+	   if(cont5m>=3) { cout << "Masses lletres M seguides!!!" << endl; return 0; }
+	   if(debug==1)cout << "V suma 5000 result =" << result << endl;	   
+	}
+      }else if (roman[i] == 'I')      { result +=    1; }
+      else if (roman[i] == 'V')      { 
+	if(roman[i-1]!='I' && roman[i+1]=='I')result +=    5;
+	else {
+	  if(i+1<=LEN){
+	    cout << "mal format V" << endl;
+	    result=0;	  
+	    return 0;
+	    }
+	}
+      }
+      else if (roman[i] == 'X')      { result +=   10; }
+      else if (roman[i] == 'L')      { 
+	if(roman.find_first_of('L')== roman.find_last_of('L'))	result +=   50; 
+	else {
+	  cout << "L mal posicionada" << endl;
+	  return 0;
+	}	
+      }
+      else if (roman[i] == 'C')      { result +=  100; }
+      else if (roman[i] == 'D'){
+	if(roman.find_first_of('D')==roman.find_last_of('D'))      { result +=  500; }
+	else {
+	  cout << "D mal posicionada" << endl;
+	  return 0;
+	}
+      }
+      else if (roman[i] == 'M')      { result += 1000; }
+      else if (roman[i] == '\u01B0')     {result += 5000; }
+      else { 
+	cout << "número erroni"; 
+	cout << " roman[i] = " << roman[i] << " \"\u01B0\"=" << "\u01B0" << endl;
+	return 0;	
+      } 
       
-      return (valor);
-    }	
-};
-    
-  
-int main() 
-{ 
-   Roma roma;
-    
-    roma.set_num();
-    if(roma.as_int()!=-15) cout << " Roman " << roma.get_num() << " equals " << roma.as_int() << endl;
-    else cout << " número mal format!!!" << endl;
-   
-    return (0); 
+   }
+   return result;
+}
+int main()
+{
+
+	string test = "\u01B0";//"MMMDCCCXCIX"; 
+	cout << "MMMDCCCXCIX = " << roman2decimal(test) << endl;
+	
+	return 0;
 }
